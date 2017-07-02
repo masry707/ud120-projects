c2a48db04bc50d5d16494e2f304f485c7728e3a0
Ahmed El Masry <ahmed.elmasry707@icloud.com>
2017-07-02 15:52:26 +0200

    completed: mini project lesson 7
    Signed-off-by: Ahmed El Masry <ahmed.elmasry707@icloud.com>

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -17,19 +17,19 @@ import sys
 import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+dictionary = pickle.load(open("../final_project/final_project_dataset_modified.pkl", "rb"))
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
-target, features = targetFeatureSplit( data )
+data = featureFormat(dictionary, features_list, remove_any_zeroes=True, sort_keys='../tools/python2_lesson06_keys.pkl')
+target, features = targetFeatureSplit(data)
 
 ### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,7 +37,19 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
 
+print('\n ******* Coefficient (Slope) ******** \n')
+print(reg.coef_[0])
+print('\n ******* intercept_ ******** \n')
+print(reg.intercept_)
+print('***** Stats on Training dataset *****')
+print('r-squared score: ', reg.score(feature_train, target_train))
+
+print('***** Stats on Test dataset *****')
+print('r-squared score: ', reg.score(feature_test, target_test))
 
 
 
@@ -48,9 +60,9 @@ test_color = "b"
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
 for feature, target in zip(feature_test, target_test):
-    plt.scatter( feature, target, color=test_color ) 
+    plt.scatter(feature, target, color=test_color)
 for feature, target in zip(feature_train, target_train):
-    plt.scatter( feature, target, color=train_color ) 
+    plt.scatter(feature, target, color=train_color)
 
 ### labels for the legend
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
@@ -61,9 +73,14 @@ plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
 ### draw the regression line, once it's coded
 try:
-    plt.plot( feature_test, reg.predict(feature_test) )
+    plt.plot(feature_test, reg.predict(feature_test))
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+print('\n ******* Coefficient (Slope) of new regression line ******** \n')
+print(reg.coef_[0])
+plt.plot(feature_train, reg.predict(feature_train), color="b")
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
